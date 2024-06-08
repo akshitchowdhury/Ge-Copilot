00e514069496e635c04702ee1f885f0a68d09e59
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2024-06-09 01:50:43 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -98,7 +98,7 @@ const App = () => {
           </div>
         )}
 
-        {loading && <Loading />}
+        {loading && <Loading darkMode={darkMode}/>}
 
         {offline && (
           <Error
